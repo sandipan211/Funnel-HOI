2ee62f469b4877e3bda719d95fe5b57c331db576
Sandipan Sarma <sandipan.sarma@iitg.ac.in>
2025-03-07 17:25:21 +0530

    Add files via upload

---
+++ ALIP/src/open_alip/my_alip_load.py
@@ -0,0 +1,77 @@
+import torch
+from . import create_model, factory, tokenize
+import argparse
+from PIL import Image
+import torch.nn.functional as F
+import torch.nn as nn
+import numpy as np
+
+def get_state_dict(model_weight):
+    state_dict = torch.load(model_weight)
+    state_dict_removed = {}
+    for k, value in state_dict.items():
+        if "module." in k:
+            k_removed = k.split("module.")[-1]
+            state_dict_removed[k_removed] = value
+        else:
+            state_dict_removed[k] = value
+    return state_dict_removed
+
+def get_transform(input_size=224):
+    # original ALIP code demands two slightly different transforms for train and test - but we keep it as test mode always, since it then becomes identical to CLIP's transforms
+
+    transform = factory.image_transform(input_size, 
+                                        is_train=False,
+                                        mean=(0.48145466, 0.4578275, 0.40821073),
+                                        std= (0.26862954, 0.26130258, 0.27577711))
+    return transform
+
+def load(model_name, model_weight):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model_alip = create_model(model_name)
+    state_dict = get_state_dict(model_weight)
+    model_alip.load_state_dict(state_dict, strict=True)
+    model_alip.to(device)
+    transform = get_transform()
+    return model_alip, transform
+
+def main(args):
+
+    print('Original ALIP (my version)')
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = load(args.model_name, args.model_weight)
+    image = preprocess(Image.open("HICO_test2015_00000001.jpg")).unsqueeze(0).to(device)
+    text = tokenize(["a photo of a horse"]).to(device)
+    logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+    with torch.no_grad():
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(text)
+        print(f'img ftrs = {image_features.shape}')
+        print(f'txt_ftrs = {text_features.shape}')
+
+        image_features = F.normalize(image_features, dim=-1)
+        text_features = F.normalize(text_features, dim=-1).mean(dim=0)
+        text_features /= text_features.norm()
+        logits = 100. * image_features @ text_features
+
+        # taken from ModifiedCLIP - incomplete - will do later
+        logit_scale = logit_scale.exp()
+        logits_per_image = logit_scale * image_features @ text_features.t()
+        logits_per_text = logits_per_image.t()
+
+    print(f'Similarity: {logits}')
+
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="ZeroShot")
+    # parser.add_argument("--batch-size", default=128, type=int)
+    # parser.add_argument("--dataset", default="cifar10", type=str)
+    parser.add_argument("--model_name", default="ViT-B/32", help="Name of the vision backbone to use.")
+    parser.add_argument("--model_weight", default="", type=str, help="pretrain model weight path.")
+    # parser.add_argument("--input-size", default=224, type=int, help="Image resolution.")
+    # parser.add_argument("--output-file", default="", type=str, help="output results file")
+
+    args = parser.parse_args()
+    main(args)
